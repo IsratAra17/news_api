d477e24fc4edd96748510cd85f5b723a75696adf
s.m.israt <smisratara25@gmail.com>
2023-03-30 08:30:25 +0600

    1.importing the http package: import 'package:http/http.dart' as http; 2.copy the api url in a string. 3.The data we get from server ,put it in "response" variable through http request . 4.then Decode the json code (only the body[response.body] )and put it into variable named 'data'. 5.write print function to check our response. 6.Now create  an obj and parse the data through model class "newsModel=NewsModel.fromJson(data);"
    //model class a data pass kore dibo
    7.Call the fetchHomeData() to init state() method.

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_api/model/news_model.dart';
 
 class Home_page extends StatefulWidget {
   const Home_page({Key? key}) : super(key: key);
@@ -8,6 +12,30 @@ class Home_page extends StatefulWidget {
 }
 
 class _Home_pageState extends State<Home_page> {
+
+  String url="https://newsapi.org/v2/everything?q=bitcoin&sortBy=relevency&pageSize=5andpage=1&apiKey=0ca84d47b3184231ad3ab2c9c2cf033b";
+
+  //create an obj
+  NewsModel ?newsModel;
+
+
+  fetchHomeData()async{
+    var response=await http.get(Uri.parse(url));//api hit korlam "response" a raklam
+     var data=jsonDecode((response.body));
+     print("our response is ${data}");
+//model class a data pass kore dibo
+     newsModel=NewsModel.fromJson(data);
+     setState(() {
+
+     });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    fetchHomeData();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold();
