19e349181ef64bfbad67e26ae95fae58b23a19bb
s.m.israt <smisratara25@gmail.com>
2023-04-07 22:59:42 +0600

    update

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_api/model/news_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
 
 class Home_page extends StatefulWidget {
   const Home_page({Key? key}) : super(key: key);
@@ -38,6 +40,93 @@ class _Home_pageState extends State<Home_page> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("news App"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+      body: Container(
+          padding: EdgeInsets.all(12),
+          width: double.infinity,
+          child: ListView(
+            children: [
+              FutureBuilder <NewsModel>(
+                future: fetchHomeData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("Something is wrong");
+                  } else if (snapshot.data == null) {
+                    return Text("snapshot data are null");
+                  }
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: snapshot.data!.articles!.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        color: Colors.white,
+                        height: 130,
+                        padding: const EdgeInsets.all(8.0),
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: 60,width: 60,
+                              color: Colors.blueGrey,
+                            ),
+                            Positioned(
+                              right: 0,bottom: 0,
+                              child: Container(
+                                height: 50,width: 50,
+                                color: Colors.blueGrey,
+                              ),),
+                            Container(
+                              color: Colors.white,
+                              padding: EdgeInsets.all(14),
+                              margin: EdgeInsets.all(14),
+                              child: Row(
+
+                                children: [
+                                  Expanded(
+                                    flex:3,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(6),
+                                      child:CachedNetworkImage(
+                                        imageUrl: "${snapshot.data!.articles![index].urlToImage}",
+                                        placeholder: (context, url) => CircularProgressIndicator(),
+                                        errorWidget: (context, url, error) => Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOmYqa4Vpnd-FA25EGmYMiDSWOl9QV8UN1du_duZC9mQ&s"),
+                                      ),
+                                      //Image(image: NetworkImage("${snapshot.data!.articles![index].urlToImage}",))
+                                    ),
+                                  ),
+                                  SizedBox(width: 8,),
+                                  Expanded(
+                                      flex: 10,
+                                      child:Column(
+                                        children: [
+                                          Text(
+                                            "${snapshot.data!.articles![index].title}",maxLines: 2,),
+
+                                          Text(
+                                              "${snapshot.data!.articles![index].publishedAt}")
+                                        ],
+                                      )
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              )
+            ],
+          )),
+    );
   }
 }
